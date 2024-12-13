52db7a1a29181804eed04100bd9f02800a3db049
pradyoth138 <116701365+pradyoth138@users.noreply.github.com>
2024-12-13 14:15:10 +0530

    Add files via upload

---
+++ staticconcept/staticblockmethod/MyClassDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.staticconcept.staticblockmethod;
+
+public class MyClassDemo {
+	public static void main(String[] args) {
+		MyClass o1 = new MyClass();
+		System.out.println(o1);
+		
+		MyClass o2 = new MyClass();
+		System.out.println(o2);
+
+	}
+
+}
